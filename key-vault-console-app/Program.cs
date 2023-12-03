83918c80db2dff8c97d18e471f95d1d2521a602d
Jerry Nettleton <jnettleton@gmail.com>
2023-12-03 01:26:05 -0600

    save project

--- key-vault-console-app/Program.cs
+++ key-vault-console-app/Program.cs
@@ -1,16 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//#define ENABLE_USER_SECRETS
+
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+#if ENABLE_USER_SECRETS
+using key_vault_console_app;
+#endif
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // NOTE: need to set up Azure account for local development
+        // https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication/local-development-dev-accounts
 
-string keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
-var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+#if ENABLE_USER_SECRETS
+        // NOTE: use "Manage User Secrets" to store secrets
+        var secretAppsettingReader = new SecretAppsettingReader();
+        var secretValues = secretAppsettingReader.ReadSection<SecretValues>("KeyVault");
+#endif
 
-var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+        var vaultName = "web-demo-kv";
+        var vaultUrl = "https://" + vaultName + ".vault.azure.net";
 
-var secretClientId = await client.GetSecretAsync("client-id");
-var secretClientSecret = await client.GetSecretAsync("client-secret");
+        // Create a new secret client using the default credential from Azure.Identity using environment variables previously set,
+        // including AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, and AZURE_TENANT_ID.
+        var client = new SecretClient(vaultUri: new Uri(vaultUrl), credential: new DefaultAzureCredential());
 
-Console.WriteLine($"client-id     = '{secretClientId.Value.Value}'.");
-Console.WriteLine($"client-secret = '{secretClientSecret.Value.Value}'.");
+        // Retrieve a secret using the secret client.
+        var secretClientId = client.GetSecret("client-id");
+        var secretClientSecret = client.GetSecret("client-secret");
+
+    }
+}
+
+#if ENABLE_USER_SECRETS
+public class SecretValues
+{
+    public string TenantId { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+}
+#endif
